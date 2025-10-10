90ec52d1ace37babd6ed58ce913e6b63b59543a2
logicwaller <a1370124587@163.com>
2025-10-10 21:23:12 +0800

    proj1-部分完成task2,运行时偶尔会发生段错误

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -17,11 +17,6 @@
 namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
-  // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
-
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
 }
@@ -34,8 +29,29 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  std::optional<DiskRequest> result;
+  // 以下都必须用std::move,因为DiskRequest中的std::promise不可拷贝,故需要用move直接移动值
+  result.emplace(std::move(r));
+  request_queue_.Put(std::move(result));
+}
 
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  while (true) {  // TODO:什么时候停止
+    std::optional<DiskRequest> request = request_queue_.Get();
+    // 当读取值为空时结束进程
+    if (!request.has_value()) return;
+
+    DiskRequest request_value(std::move(request.value()));
+    // 进行读/写操作
+    if (request_value.is_write_) {
+      disk_manager_->WritePage(request_value.page_id_, request_value.data_);
+    } else {
+      disk_manager_->ReadPage(request_value.page_id_, request_value.data_);
+    }
+    // 完成后将request中的promise传回值
+    request_value.callback_.set_value(true);
+  }
+}
 
 }  // namespace bustub
